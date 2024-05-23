6d07e62f08acd4ff57f158eb007e1be9624392ab
hoo01 <497458301@qq.com>
2024-05-23 16:36:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ os.system(f'git clone https://code.openxlab.org.cn/hoo01/hoo01_robot2.git {base_
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True,torch_dtype=torch.float16)
 
 def chat(message,history):
     for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
@@ -18,6 +18,6 @@ def chat(message,history):
 gr.ChatInterface(chat,
                  title="hoo01_robot2",
                 description="""
-hoo01'robot is talking to you.  
+hoo01_robot2 is talking to you.  
                  """,
                  ).queue(1).launch()
